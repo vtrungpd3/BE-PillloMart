2886e1f63dd3c16e80474edba7695a5e45bd2b98
vtrungpd3 <v.trungpd3@cloudhms.net>
2021-09-02 19:34:53 +0700

    [Feature] Mongo Atlas

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const { MONGODB } = require('../utils/config');
 
+const uri = "mongodb+srv://admin:admin@pillowmart.pv3ig.mongodb.net/pillloMart?retryWrites=true&w=majority"
+
 const ConnectDB = async () => {
     try {
-        await mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+        await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('MongoDB is connected');
     } catch (err) {
         console.error(err.message);
