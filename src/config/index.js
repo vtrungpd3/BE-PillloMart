b02b34c98fd5f79ec321ed3d66eed23d8f3f099c
vtrungpd3 <v.trungpd3@cloudhms.net>
2021-09-25 17:59:31 +0700

    [Update] Refactor code

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
-const development = require('./development');
-const test = require('./test');
+const devEnv = require('./devEnv');
+const testEnv = require('./testEnv');
 
-let config = process.env.NODE_ENV === 'test' ? test : development;
+let config = process.env.NODE_ENV === 'test' ? testEnv : devEnv;
 
 module.exports = config;
